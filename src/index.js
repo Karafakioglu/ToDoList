5846c9bf2bc8bcf643c6883e3057bdd63b4dd448
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-12-05 00:02:13 +0100

    finish the front-end of the application

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import logo from './assets/logo.svg';
 import projectLogo from './assets/project-logo.svg';
 import addNewProjectLogo from './assets/new-project-logo.svg';
 import deleteProjectLogo from './assets/delete-logo.svg';
+import doneLogo from './assets/done-logo.svg';
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logo;
@@ -16,8 +17,54 @@ projectLogoImgs.forEach(img => {
 const addNewProject = document.getElementById('new-project-logo');
 addNewProject.src = addNewProjectLogo;
 
+
 const deleteLogoElems = document.querySelectorAll('.delete-logo');
 deleteLogoElems.forEach(elem =>{
     elem.src = deleteProjectLogo;
+
+})
+
+const doneLogoElems = document.querySelectorAll('.done-logo');
+doneLogoElems.forEach(elem =>{
+    elem.src = doneLogo;
+})
+
+const deleteTaskLogoElems = document.querySelectorAll('.delete-task-logo');
+deleteTaskLogoElems.forEach(elem =>{
+    elem.src = deleteProjectLogo;
+})
+
+
+const projectElems = document.querySelectorAll('.project-button');
+projectElems.forEach(elem =>{
+    elem.addEventListener('click', e =>{
+        alert('You clicked on the project')
+    })
+})
+
+deleteLogoElems.forEach(elem =>{
+    elem.addEventListener('click', e =>{
+        alert("you clicked on project delete button")
+    })
+})
+
+const newProjectButtonElem = document.getElementById('new-project-div');
+newProjectButtonElem.addEventListener('click', e =>{
+    alert('You clicked on  add new project button')
+})
+
+
+const taskDoneButtonElems = document.querySelectorAll('.task-done-button');
+taskDoneButtonElems.forEach(elem => {
+    elem.addEventListener('click', e =>{
+        alert('you clicked on done task btn');
+    })
+})
+
+const deleteTaskButtonElems = document.querySelectorAll('.delete-task-logo');
+deleteTaskButtonElems.forEach(elem => {
+    elem.addEventListener('click', e =>{
+        alert("you clicked on task delete button");
+    })
 })
 // helloWorld();
