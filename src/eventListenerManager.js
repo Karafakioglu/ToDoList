c809dba1cf053a95a294bfa0db65828498f1c81a
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-12-23 23:26:39 +0100

    fix newProjectInput issue

--- src/eventListenerManager.js
+++ src/eventListenerManager.js
@@ -1,3 +1,9 @@
+const newTaskAddButton = document.getElementById("task-add-button-popup");
+const newTaskInput = document.querySelector("[data-new-task-input]");
+
+const newProjectInput = document.querySelector("[data-new-project-input]");
+const newProjectAddButton = document.getElementById("project-add-button-popup");
+
 export function createNewProject(e, createItem, projects, saveAndRender) {
   e.preventDefault();
   let projectName = newProjectInput.value;
